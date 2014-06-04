d1b9a3f6be761446b435f6df6eaffed997e3c30f
Edward Kenworthy <Edward@Esteldin.local>
2014-06-04 19:07:01 +0100

    Initial Commit

---
+++ Swift XBlasterTests/Swift_XBlasterTests.swift
@@ -0,0 +1,35 @@
+//
+//  Swift_XBlasterTests.swift
+//  Swift XBlasterTests
+//
+//  Created by Edward Kenworthy on 04/06/2014.
+//  Copyright (c) 2014 Himeji Heavy Industry. All rights reserved.
+//
+
+import XCTest
+
+class Swift_XBlasterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
